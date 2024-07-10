5e2a2047807a173dc495db31750597b599d9ef81
Robles <alejandropsm2@gmail.com>
2024-07-10 14:50:40 -0600

    first commit

--- src/app/cbd/page.jsx
+++ src/app/cbd/page.jsx
@@ -13,8 +13,6 @@ import cbd_picture_3 from "../img/cbd_picture_3.jpg";
 import cbd_picture_4 from "../img/cbd_picture_4.jpg";
 import cbd_picture_logo from "../img/cbd_picture_logo.jpg";
 
-
-
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 export default function Page() {
     return (
@@ -34,8 +32,6 @@ export default function Page() {
     );
 }
 
-
-
 export const BarraPrinc = () => {
     return (
         <>
@@ -43,20 +39,32 @@ export const BarraPrinc = () => {
                 <Image
                     src={cbd_picture_logo}
                     className="img_cbd_barra"
-               
                     alt="Big logo"
                 />
 
                 <div className="logos_barra">
-                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-cart-check" viewBox="0 0 16 16">
-  <path d="M11.354 6.354a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0z"/>
-  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
-</svg>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="26"
+                        height="26"
+                        fill="currentColor"
+                        class="bi bi-cart-check"
+                        viewBox="0 0 16 16"
+                    >
+                        <path d="M11.354 6.354a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0z" />
+                        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+                    </svg>
                     &nbsp; &nbsp; &nbsp;
-                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-</svg>
-                   
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="26"
+                        height="26"
+                        fill="currentColor"
+                        class="bi bi-search"
+                        viewBox="0 0 16 16"
+                    >
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                    </svg>
                 </div>
 
                 <ul className="menu_barra" id="lista_barra_princ">
@@ -96,10 +104,10 @@ export const BarraPrinc = () => {
 export const PictureDeCarusel1 = () => {
     return (
         <div>
-            {" "}
+            
             <Image
                 src={cbd_picture_4}
-              className="picture_cbd_handle"
+                className="picture_cbd_handle"
                 alt="Big logo"
             />
         </div>
@@ -109,7 +117,7 @@ export const PictureDeCarusel1 = () => {
 export const PictureDeCarusel2 = () => {
     return (
         <div>
-            {" "}
+            
             <Image
                 src={cbd_picture_1}
                 className="picture_cbd_handle"
@@ -122,10 +130,10 @@ export const PictureDeCarusel2 = () => {
 export const PictureDeCarusel3 = () => {
     return (
         <div>
-            {" "}
+            
             <Image
                 src={cbd_picture_2}
-               className="picture_cbd_handle"
+                className="picture_cbd_handle"
                 alt="Big logo"
             />
         </div>
@@ -177,19 +185,18 @@ export const Message = () => {
     return (
         <>
             <div className="mensaje_box">
-                <div className="mensaje_handle">
-                    <h1>Cbd</h1>
-                    <p>
-                        Hemos sido pioneros en producción, distribución y
-                        comercialización de productos enfocados al cuidado
-                        personal y bienestar. Hoy consolidamos nuestra historia
-                        al lanzar al mercado la primera línea de suplementos
-                        alimenticios con CBD en nuestro país.
-                    </p>
-                    <button type="button" class="btn btn-success">
-                        Saber mas
-                    </button>
-                </div>
+                <h1>Cbd</h1>
+                <p>
+                    Hemos sido pioneros en producción, distribución y
+                    comercialización de productos enfocados al cuidado personal
+                    y bienestar. Hoy consolidamos nuestra historia al lanzar al
+                    mercado la primera línea de suplementos alimenticios con CBD
+                    en nuestro país.
+                </p>
+                <button type="button" class="btn btn-success">
+                    Saber mas
+                </button>
+
                 <Image
                     className="mensaje_img_handle"
                     src={cbd_picture_3}
@@ -245,13 +252,27 @@ export const FoTer = () => {
                         </div>
                         NewsLetter
                         <br />
-                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
-  <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
-</svg>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="26"
+                            height="26"
+                            fill="currentColor"
+                            class="bi bi-instagram"
+                            viewBox="0 0 16 16"
+                        >
+                            <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
+                        </svg>
                         &nbsp;
-                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
-  <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
-</svg>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="26"
+                            height="26"
+                            fill="currentColor"
+                            class="bi bi-twitter-x"
+                            viewBox="0 0 16 16"
+                        >
+                            <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
+                        </svg>
                         &nbsp;&nbsp;
                     </div>
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,14 +3,12 @@ import React from "react";
 import Link from "next/link";
 
 import "bootstrap/dist/css/bootstrap.css";
-import './style-page.css';
+import "./style-page.css";
 
 import Image from "next/image";
 import ident_picture_1 from "./img/ident_picture_1.png";
 import ident_picture_2 from "./img/ident_picture_2.png";
 import ident_picture_3 from "./img/ident_picture_3.png";
-
-
 
 export default function Home() {
     return (
@@ -19,9 +17,8 @@ export default function Home() {
             <InformColumn />
             <Projects_1 />
             <Projects_2 />
-            
+
             <FoTer />
-          
         </div>
     );
 }
@@ -30,12 +27,11 @@ export const FaceElements = () => {
     return (
         <>
             <header className="face_elements">
-                <div class="wrapper">
-                    <div class="column">
+                <div className="containerbox">
+                    <div className="insidebox">
                         <div>
                             <Image
                                 src={ident_picture_1}
-                              
                                 className="handle_img_faceelements"
                                 alt="big"
                             />
@@ -43,111 +39,51 @@ export const FaceElements = () => {
                         <h4>Jose Alejandro Robles</h4>
                         <h3>Web Developer, Designer</h3>
 
-                        <a href="#f" download="mi-archivo.pdf"> DownLoad CV</a>
-                        
+                        <a href="#f" download="mi-archivo.pdf">
+                            
+                            DownLoad CV
+                        </a>
                     </div>
                 </div>
             </header>
         </>
     );
 };
-/*
 
-
-
-
-
-
-
-
-export const InformColumn = () => {
-    return (
-        <>
-            <article className="_info_column_handle">
-                <div class="container text-center">
-                    <div class="row align-items-center">
-                        <div class="col">
-                            <li>
-                                I´m a web developer based in CDMX, with a
-                                passion to build digital services focus in
-                                solve complex code problems. In 2022 I started
-                                my training self-taught as a front end
-                                developer. In 2024 I Finished my engineering
-                                degree in computer systems.UTEL{" "}
-                            </li>
-                        </div>
-                        <div class="col">
-                            <li>
-                                <u>Languages I speak:</u>
-                            </li>
-
-                            <li>HTML, Js, CSS, Git</li>
-                            <br />
-                            <li>
-                                <u> Design Tools:</u>
-                            </li>
-                            <li> Affinity Designer & Figma Pen </li>
-                            <br />
-                            <li>
-                                <u> Dev Tools:</u>
-                            </li>
-                            <li> VsCode, Bootstrap, Github</li>
-                        </div>
-                        <div class="col">
-                            <li> Things I enjoy designing:</li>
-                            <li>UX, UI, Web, Apps, Logos</li>
-                            When im not online, i loves make music and visual
-                            design.
-                        </div>
-                    </div>
-                </div>
-            </article>
-        </>
-    );
-};
-
-
- */
 export const InformColumn = () => {
     return (
         <>
             <article className="_info_column_handle">
                 <div className="paquet">
                     <div>
-                        <br/><br/>
-                          I´m a web developer based in CDMX, with skills to solve  problems using programming codes. In 2024 I Finished my engineering
-                 
-                    degree in computer systems.
-                       
+                        <br />
+                        <br />
+                        I´m a web developer based in CDMX, with skills to solve
+                        problems using programming codes. In 2024 I Finished my
+                        engineering degree in computer systems.
                     </div>
                     <div>
-                      
-           <li>
-                            Languages I speak:
-                        </li>
+                        <li>Languages I speak:</li>
                         <li>HTML, Js, CSS, Git</li>
                         <br />
-                        <li>
-                         Design Tools:
-                        </li>
+                        <li>Design Tools:</li>
                         <li> Affinity Designer & Figma Pen </li>
                         <br />
-                        <li>
-                             Dev Tools:
-                        </li>
+                        <li>Dev Tools:</li>
                         <li>React, VsCode, Bootstrap, Github</li>
-                          <br/><li> Things I enjoy designing:</li>
+                        <br />
+                        <li> Things I enjoy designing:</li>
                         <li>UX, UI, Web, Apps, Logos</li>
-                  
                     </div>
                     <div>
-                    <br/><br/>
-                      
-                        <li>I choose to learn day by day to nurture soft and technical skills necessary for digital services
+                        <br />
+                        <br />
 
-
-                        When im not online, i loves make music and visual
-                        design.</li>
+                        <li>
+                            I choose to learn day by day to nurture soft and
+                            technical skills necessary for digital services When
+                            im not online, i loves make music and visual design.
+                        </li>
                     </div>
                 </div>
             </article>
@@ -159,28 +95,23 @@ export const Projects_1 = () => {
     return (
         <>
             <div className="cbdproject">
-                <div class="row justify-content-evenly">
-                    <div class="col-4">
-                        <Image
-                            src={ident_picture_2}
-                            className="imagendecbd_handle "
-                            alt="big"
-                        />
-                    </div>
-                    <div class="col-4">
-                        <h4>CBD E-commerce</h4>
-                        <p className="textcdb">
-                        A simple sketch to display 
-                        information about an online 
+                <div className="fila_1">
+                    <Image
+                        src={ident_picture_2}
+                        className="imagendecbd_handle "
+                        alt="big"
+                    />
+                </div>
+                <div className="fila_2">
+                    <h4>CBD commerce</h4>
+                    <p>
+                        A simple sketch to display information about an online
                         cbd-store on screen.
-
-
-                        </p>
-                        <p>
-                            {" "}
-                            <Link href="/cbd">See more</Link>
-                        </p>
-                    </div>
+                    </p>
+                    <p>
+                        
+                        <Link href="/cbd">See more</Link>
+                    </p>
                 </div>
             </div>
         </>
@@ -191,29 +122,23 @@ export const Projects_2 = () => {
     return (
         <>
             <div className="revanchaproject">
-                <div class="row justify-content-evenly">
-                    <div class="col-4">
-                        <h5>Revancha</h5>
-                        <p className="txtrevancha">
-                        A simple mockup to display 
-                        different blogs and notes 
+                <div className="fila_2">
+                    <h5>Revancha</h5>
+                    <p>
+                        A simple mockup to display different blogs and notes
                         belonging to a news-page on the screen.
-
-
-                        </p>
-                        <p>
-                            {" "}
-                            <Link href="/revancha">See more</Link>
-                        </p>
-                    </div>
-                    <div class="col-4">
-                        <Image
-                            src={ident_picture_3}
-                          
-                            className="imagenrevancha2"
-                            alt="big"
-                        />
-                    </div>
+                    </p>
+                    <p>
+                    
+                        <Link href="/revancha">See more</Link>
+                    </p>
+                </div>
+                <div className="fila_1">
+                    <Image
+                        src={ident_picture_3}
+                        className="imagenrevancha2"
+                        alt="big"
+                    />
                 </div>
             </div>
         </>
@@ -223,44 +148,55 @@ export const Projects_2 = () => {
 export const FoTer = () => {
     return (
         <>
-        
             <div className="handle_pagefoter">
-    <p>     
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
-<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
-</svg>
-    
-&nbsp;&nbsp;
-    
-    
-    <a href="https://github.com/Alejandro-Rob23">Alejandro-Rob23</a>
- 
-    </p>
-    <p>
-    
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
-</svg>
-&nbsp;&nbsp;
-
-<a href="https://www.linkedin.com/in/alejandro-robles-s%C3%A1nchez-2a638b27a/">Jose Alejandro Robles Sanchez</a>
- 
- 
-</p>
-<p>
-
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-arrow-up-fill" viewBox="0 0 16 16">
-<path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zm.192 8.159 6.57-4.027L8 9.586l1.239-.757.367.225A4.49 4.49 0 0 0 8 12.5c0 .526.09 1.03.256 1.5H2a2 2 0 0 1-1.808-1.144M16 4.697v4.974A4.5 4.5 0 0 0 12.5 8a4.5 4.5 0 0 0-1.965.45l-.338-.207z"/>
-<path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.354-5.354 1.25 1.25a.5.5 0 0 1-.708.708L13 12.207V14a.5.5 0 0 1-1 0v-1.717l-.28.305a.5.5 0 0 1-.737-.676l1.149-1.25a.5.5 0 0 1 .722-.016"/>
-</svg>
-
-&nbsp;&nbsp;
-
-<a href="#">alejandro_contact@joserobles.com</a></p>
-
-  </div>
+                <p>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-github"
+                        viewBox="0 0 16 16"
+                    >
+                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                    </svg>
+                    &nbsp;&nbsp;
+                    <a href="https://github.com/Alejandro-Rob23">
+                        Alejandro-Rob23
+                    </a>
+                </p>
+                <p>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-linkedin"
+                        viewBox="0 0 16 16"
+                    >
+                        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
+                    </svg>
+                    &nbsp;&nbsp;
+                    <a href="https://www.linkedin.com/in/alejandro-robles-s%C3%A1nchez-2a638b27a/">
+                        Jose Alejandro Robles Sanchez
+                    </a>
+                </p>
+                <p>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-envelope-arrow-up-fill"
+                        viewBox="0 0 16 16"
+                    >
+                        <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zm.192 8.159 6.57-4.027L8 9.586l1.239-.757.367.225A4.49 4.49 0 0 0 8 12.5c0 .526.09 1.03.256 1.5H2a2 2 0 0 1-1.808-1.144M16 4.697v4.974A4.5 4.5 0 0 0 12.5 8a4.5 4.5 0 0 0-1.965.45l-.338-.207z" />
+                        <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.354-5.354 1.25 1.25a.5.5 0 0 1-.708.708L13 12.207V14a.5.5 0 0 1-1 0v-1.717l-.28.305a.5.5 0 0 1-.737-.676l1.149-1.25a.5.5 0 0 1 .722-.016" />
+                    </svg>
+                    &nbsp;&nbsp;
+                    <a href="#">alejandro_contact@joserobles.com</a>
+                </p>
+            </div>
         </>
     );
 };
-
-
